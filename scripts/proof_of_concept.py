31ab48857cf961196648b7c2184abd8642207ed9
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-12-04 20:24:00 -0800

    Removed Unnecessary Comment

--- scripts/proof_of_concept.py
+++ scripts/proof_of_concept.py
@@ -80,7 +80,7 @@ frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can also use 'XVID' or 'MJPG'
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4', fourcc, fps, (frame_width, frame_height))
 
 while cap.isOpened():
